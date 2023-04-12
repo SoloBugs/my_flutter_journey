843c02bb7319973475a4fc9ac115f2e1863b80ad
Alejandro <novalejsh95@gmail.com>
2023-04-12 17:26:31 -0400

    proyecto toktik

---
+++ toktik/lib/config/theme/app_theme.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+//Configuration site for styles on flutter appss
+
+class AppTheme {
+  ThemeData getTheme() => ThemeData(
+    useMaterial3:  true,
+    brightness: Brightness.dark
+
+  );
+}
