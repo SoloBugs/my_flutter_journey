4ae32d9e681de0f8ed9adce5e1d877f21433b377
alejandro <novalejsh95@gmail.com>
2023-05-04 21:30:21 -0400

    cambios en el datasource y repositories

--- toktik/lib/main.dart
+++ toktik/lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/infrastructure/datasources/local_video_datasource_impl.dart';
+import 'package:toktik/infrastructure/repositories/video_post_repositories_impl.dart';
 import 'package:toktik/presentation/providers/discover_provider.dart';
 import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
@@ -11,15 +13,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final videoPostRepository =
+        VideoPostsRepositoryImpl(videosDatasource: LocalVideoDataSource());
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
           lazy: false,
-          create: (_) => 
-            DiscoverProvider()
-              ..loadNextPage(),
+          create: (_) => DiscoverProvider(videoRepository: videoPostRepository)
+            ..loadNextPage(),
         )
-        ],
+      ],
       child: MaterialApp(
         title: 'Material App',
         debugShowCheckedModeBanner: false,
