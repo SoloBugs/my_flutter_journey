78080951ec0da8ad12e84be527549cac591e36ff
Alejandro <novalejsh95@gmail.com>
2023-05-12 17:24:53 -0400

    exportacion de screens para rutas

--- witgets_app/lib/presentation/screens/screens.dart
+++ witgets_app/lib/presentation/screens/screens.dart
@@ -1,3 +1,10 @@
+export 'package:flutter/material.dart';
+export 'package:witgets_app/presentation/screens/app_tutorial/app_tutorial_screens.dart';
+export 'package:witgets_app/presentation/screens/infinite_scroll/infinite_scoll.dart';
+export 'package:witgets_app/presentation/screens/progress_screen/progress_screen.dart';
+export 'package:witgets_app/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:witgets_app/presentation/screens/ui_controls/ui_controls.dart';
+//----------------------------------------------------------------------------------------//
 export 'package:witgets_app/presentation/screens/buttons/buttons_screen.dart';
 export 'package:witgets_app/presentation/screens/cards/cards_screens.dart';
 export 'package:witgets_app/presentation/screens/home/home_screen.dart';
