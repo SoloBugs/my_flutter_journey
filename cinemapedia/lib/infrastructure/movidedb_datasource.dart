019ae06ec2bbef1e5116f92394942b78caed0e9b
Alejandro <novalejsh95@gmail.com>
2023-06-22 16:12:45 -0400

    preparacion de peticion http a moviedb

---
+++ cinemapedia/lib/infrastructure/movidedb_datasource.dart
@@ -0,0 +1,22 @@
+import 'package:cinemapedia/config/constants/enviroment.dart';
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:dio/dio.dart';
+
+import '../domain/entities/movie.dart';
+
+class MoviedbDatasource extends MovieDatasource {
+  final dio = Dio(BaseOptions(
+      baseUrl: 'https://api.themoviedb.org/3',
+      queryParameters: {
+        'api_key': Enviroment.theMovieDBkey,
+        'language': 'es-MX'
+      }));
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response = await dio.get('/movie/now_playing');
+    final List<Movie> movies = [];
+
+    return movies;
+  }
+}
--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -7,10 +7,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(Enviroment.theMovieDBkey),
-      ),
+    return const Scaffold(
+      body: Placeholder(),
     );
   }
 }
