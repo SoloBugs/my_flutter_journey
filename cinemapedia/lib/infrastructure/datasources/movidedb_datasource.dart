7c76edebd222a4a262c298ed178ca568995955ab
Alejandro <novalejsh95@gmail.com>
2023-06-23 18:22:03 -0400

    cambios provider

--- cinemapedia/lib/infrastructure/datasources/movidedb_datasource.dart
+++ cinemapedia/lib/infrastructure/datasources/movidedb_datasource.dart
@@ -1,5 +1,7 @@
 import 'package:cinemapedia/config/constants/enviroment.dart';
 import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 import '../../domain/entities/movie.dart';
@@ -15,8 +17,11 @@ class MoviedbDatasource extends MovieDatasource {
   @override
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
     final response = await dio.get('/movie/now_playing');
-    final List<Movie> movies = [];
-
+    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+    final List<Movie> movies = movieDBResponse.results
+        .where((movieDB) => movieDB.posterPath != 'no-poster')
+        .map((movieDB) => MovieMapper.movieDBToEntity(movieDB))
+        .toList();
     return movies;
   }
 }
--- cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,3 @@
-import 'package:cinemapedia/config/constants/enviroment.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
