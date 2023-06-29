9247d5f83499ee1c66fca6f422faf7ce39d251cf
Alejandro <novalejsh95@gmail.com>
2023-06-29 17:17:26 -0400

    Agregar metodo general para la respuesta de la peticion

--- cinemapedia/lib/infrastructure/datasources/movidedb_datasource.dart
+++ cinemapedia/lib/infrastructure/datasources/movidedb_datasource.dart
@@ -14,15 +14,27 @@ class MoviedbDatasource extends MovieDatasource {
         'language': 'es-MX'
       }));
 
-  @override
-  Future<List<Movie>> getNowPlaying({int page = 1}) async {
-    final response =
-        await dio.get('/movie/now_playing', queryParameters: {'page': page});
-    final movieDBResponse = MovieDbResponse.fromJson(response.data);
+  List<Movie> _jsonToMovies(Map<String, dynamic> json) {
+    final movieDBResponse = MovieDbResponse.fromJson(json);
     final List<Movie> movies = movieDBResponse.results
         .where((movieDB) => movieDB.posterPath != 'no-poster')
         .map((movieDB) => MovieMapper.movieDBToEntity(movieDB))
         .toList();
     return movies;
+  }
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) async {
+    final response =
+        await dio.get('/movie/now_playing', queryParameters: {'page': page});
+
+    return _jsonToMovies(response.data);
+  }
+
+  @override
+  Future<List<Movie>> getPopular({int page = 1}) async {
+    final response =
+        await dio.get('/movie/popular', queryParameters: {'page': page});
+    return _jsonToMovies(response.data);
   }
 }
